ff6c8f86407ddd2b8b06909d23fa29da3658225e
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-10-12 20:16:29 +0200

    Revert "python3Packages.pysnmpcrypto: 0.0.4 -> 0.1.0"
    This reverts commit b2f50708cc5efb9fa1d8efc3613ed5ecb0bb4a28.

--- pkgs/development/python-modules/pysnmpcrypto/default.nix
+++ pkgs/development/python-modules/pysnmpcrypto/default.nix
@@ -1,30 +1,42 @@
 {
   lib,
   buildPythonPackage,
+  fetchPypi,
+
+  # build-system
+  setuptools,
+
+  # dependencies
   cryptography,
-  fetchFromGitHub,
-  poetry-core,
+  pycryptodomex,
+
+  # tests
   pytestCheckHook,
-  pythonOlder,
 }:
 
 buildPythonPackage rec {
   pname = "pysnmpcrypto";
-  version = "0.1.0";
+  version = "0.0.4";
   pyproject = true;
 
-  disabled = pythonOlder "3.8";
-
-  src = fetchFromGitHub {
-    owner = "lextudio";
-    repo = "pysnmpcrypto";
-    rev = "refs/tags/v${version}";
-    hash = "sha256-gNRD8mSWVVLXwJjb3nT7IKnjTdwTutFDnQybgZTY2b0=";
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-tjX7Ox7GY3uaADP1BQYhThbrhFdLHSWrAnu95MqlUSk=";
   };
 
-  build-system = [ poetry-core ];
+  postPatch = ''
+    # ValueError: invalid literal for int() with base 10: 'post0' in File "<string>", line 104, in <listcomp>
+    substituteInPlace setup.py --replace \
+      "observed_version = [int(x) for x in setuptools.__version__.split('.')]" \
+      "observed_version = [36, 2, 0]"
+  '';
 
-  dependencies = [ cryptography ];
+  nativeBuildInputs = [ setuptools ];
+
+  propagatedBuildInputs = [
+    cryptography
+    pycryptodomex
+  ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
@@ -32,8 +44,8 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "Strong crypto support for Python SNMP library";
-    homepage = "https://github.com/lextudio/pysnmpcrypto";
-    changelog = "https://github.com/lextudio/pysnmpcrypto/blob/v${version}/CHANGES.txt";
+    homepage = "https://github.com/etingof/pysnmpcrypto";
+    changelog = "https://github.com/etingof/pysnmpcrypto/blob/${version}/CHANGES.txt";
     license = licenses.bsd2;
     maintainers = [ ];
   };
